b2cd7c554365beb49cdff38aa7e54825061f2e97
LegacyGuy <prathameshkashid26@gmail.com>
2024-06-27 15:33:44 +0530

    iteration3

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,22 +2,7 @@ package vitals;
 
 public class Main {
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        boolean isOk = true;
-
-        if (temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            isOk = false;
-        }
-        if (soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            isOk = false;
-        }
-        if (chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            isOk = false;
-        }
-
-        return isOk;
+        return !((temperature < 0) || (temperature > 45) || (soc < 20) || (soc > 80) || (chargeRate > 0.8))
     }
 
     public static void main(String[] args) {
