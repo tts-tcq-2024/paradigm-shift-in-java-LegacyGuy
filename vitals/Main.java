7bd945e3753a1b1a677ea332b27185b819037563
LegacyGuy <prathameshkashid26@gmail.com>
2024-06-27 15:35:12 +0530

    iteration4

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,7 +2,7 @@ package vitals;
 
 public class Main {
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        return !((temperature < 0) || (temperature > 45) || (soc < 20) || (soc > 80) || (chargeRate > 0.8))
+        return !((temperature < 0) || (temperature > 45) || (soc < 20) || (soc > 80) || (chargeRate > 0.8));
     }
 
     public static void main(String[] args) {
