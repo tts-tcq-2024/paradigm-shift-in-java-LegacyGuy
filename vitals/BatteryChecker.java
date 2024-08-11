3d796a400c865924db020eba0463ad2a750fac13
LegacyGuy <prathameshkashid26@gmail.com>
2024-08-11 20:32:49 +0530

    Update BatteryChecker.java

--- vitals/BatteryChecker.java
+++ vitals/BatteryChecker.java
@@ -1,77 +1,65 @@
 package vitals;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class BatteryChecker {
-    static String language = "EN"; // Default language
+    private String language;
+    private boolean enableWarningForTemperature = true;
+    private boolean enableWarningForSoc = true;
+    private boolean enableWarningForChargeRate = true;
 
-    // Messages for different languages
-    static Map<String, Map<String, String>> messages = new HashMap<String, Map<String, String>>() {{
-        put("EN", new HashMap<String, String>() {{
-            put("LOW_TEMPERATURE", "Temperature is too low");
-            put("HIGH_TEMPERATURE", "Temperature is too high");
-            put("LOW_SOC", "State of Charge is too low");
-            put("HIGH_SOC", "State of Charge is too high");
-            put("LOW_CHARGE_RATE", "Charge Rate is too low");
-            put("HIGH_CHARGE_RATE", "Charge Rate is too high");
-            put("LOW_TEMPERATURE_WARNING", "Warning: Approaching low temperature");
-            put("HIGH_TEMPERATURE_WARNING", "Warning: Approaching high temperature");
-            put("LOW_SOC_WARNING", "Warning: Approaching discharge");
-            put("HIGH_SOC_WARNING", "Warning: Approaching charge peak");
-            put("LOW_CHARGE_RATE_WARNING", "Warning: Approaching low charge rate");
-            put("HIGH_CHARGE_RATE_WARNING", "Warning: Approaching high charge rate");
-        }});
-        put("DE", new HashMap<String, String>() {{
-            put("LOW_TEMPERATURE", "Temperatur ist zu niedrig");
-            put("HIGH_TEMPERATURE", "Temperatur ist zu hoch");
-            put("LOW_SOC", "Ladezustand ist zu niedrig");
-            put("HIGH_SOC", "Ladezustand ist zu hoch");
-            put("LOW_CHARGE_RATE", "Laderate ist zu niedrig");
-            put("HIGH_CHARGE_RATE", "Laderate ist zu hoch");
-            put("LOW_TEMPERATURE_WARNING", "Warnung: Annäherung an niedrige Temperatur");
-            put("HIGH_TEMPERATURE_WARNING", "Warnung: Annäherung an hohe Temperatur");
-            put("LOW_SOC_WARNING", "Warnung: Annäherung an Entladung");
-            put("HIGH_SOC_WARNING", "Warnung: Annäherung an Ladehöhepunkt");
-            put("LOW_CHARGE_RATE_WARNING", "Warnung: Annäherung an niedrige Laderate");
-            put("HIGH_CHARGE_RATE_WARNING", "Warnung: Annäherung an hohe Laderate");
-        }});
-    }};
-
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        return isParameterOk("Temperature", temperature, 0, 45, 2.25f) &&
-               isParameterOk("State of Charge", soc, 20, 80, 4) &&
-               isParameterOk("Charge Rate", chargeRate, 0, 0.8f, 0.04f);
+    public BatteryChecker(String language) {
+        this.language = language;
     }
 
-    static boolean isParameterOk(String parameter, float value, float min, float max, float warningTolerance) {
-        return !isCritical(parameter, value, min, max) && !isWarning(parameter, value, min, max, warningTolerance);
+    public boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+        boolean temperatureOk = isParameterOk("Temperature", temperature, 0, 45, 2.25f, enableWarningForTemperature);
+        boolean socOk = isParameterOk("State of Charge", soc, 20, 80, 4, enableWarningForSoc);
+        boolean chargeRateOk = isParameterOk("Charge Rate", chargeRate, 0, 0.8f, 0.04f, enableWarningForChargeRate);
+        return temperatureOk && socOk && chargeRateOk;
     }
 
-    static boolean isCritical(String parameter, float value, float min, float max) {
+    private boolean isParameterOk(String parameter, float value, float min, float max, float warningTolerance, boolean warningEnabled) {
+        return !isCritical(parameter, value, min, max) && 
+               (!warningEnabled || !isWarning(parameter, value, min, max, warningTolerance));
+    }
+
+    private boolean isCritical(String parameter, float value, float min, float max) {
         if (value < min) {
-            System.out.println(getMessage(parameter, "LOW"));
             return true;
         } else if (value > max) {
-            System.out.println(getMessage(parameter, "HIGH"));
             return true;
         }
         return false;
     }
 
-    static boolean isWarning(String parameter, float value, float min, float max, float warningTolerance) {
-        if (value < min + warningTolerance) {
-            System.out.println(getMessage(parameter, "LOW_WARNING"));
-            return true;
-        } else if (value > max - warningTolerance) {
-            System.out.println(getMessage(parameter, "HIGH_WARNING"));
-            return true;
-        }
-        return false;
+    private boolean isWarning(String parameter, float value, float min, float max, float warningTolerance) {
+        return value < min + warningTolerance || value > max - warningTolerance;
     }
 
-    static String getMessage(String parameter, String condition) {
-        String key = condition + "_" + parameter.toUpperCase().replace(" ", "_");
-        return messages.get(language).getOrDefault(key, "Unknown condition");
+    public StringBuilder getColorManual() {
+        StringBuilder manual = new StringBuilder();
+        manual.append(LanguageSupport.getMessage(language, "Temperature", "LOW"))
+              .append("\n")
+              .append(LanguageSupport.getMessage(language, "Temperature", "HIGH"))
+              .append("\n")
+              .append(LanguageSupport.getMessage(language, "State of Charge", "LOW"))
+              .append("\n")
+              .append(LanguageSupport.getMessage(language, "State of Charge", "HIGH"))
+              .append("\n")
+              .append(LanguageSupport.getMessage(language, "Charge Rate", "LOW"))
+              .append("\n")
+              .append(LanguageSupport.getMessage(language, "Charge Rate", "HIGH"));
+        return manual;
+    }
+
+    public void setWarningForTemperature(boolean enable) {
+        this.enableWarningForTemperature = enable;
+    }
+
+    public void setWarningForSoc(boolean enable) {
+        this.enableWarningForSoc = enable;
+    }
+
+    public void setWarningForChargeRate(boolean enable) {
+        this.enableWarningForChargeRate = enable;
     }
 }
