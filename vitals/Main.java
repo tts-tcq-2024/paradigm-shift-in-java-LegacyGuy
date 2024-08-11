a4101c668aa3920fdc1d54fba1142ba6db839980
LegacyGuy <prathameshkashid26@gmail.com>
2024-08-11 20:42:29 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,9 +2,13 @@ package vitals;
 
 public class Main {
     public static void main(String[] args) {
-        BatteryChecker.language = "EN"; // Switch to "DE" for German
-        assert (BatteryChecker.batteryIsOk(25, 70, 0.7f) == true);
-        assert (BatteryChecker.batteryIsOk(50, 85, 0.0f) == false);
+        BatteryChecker checker = new BatteryChecker("EN");
+        
+        // Test cases
+        assert (checker.batteryIsOk(25, 70, 0.7f));
+        assert (!checker.batteryIsOk(50, 85, 0.0f));
+
         System.out.println("All tests passed.");
     }
 }
+
